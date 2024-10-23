e7dc8a906fa3b0e1e0ce64ad53a5a94eaf5105e4
Matthewh1225 <mhuckman97@yahoo.com>
2024-10-23 11:58:56 -0500

    replace files

---
+++ Question1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Node node1 =new Node(args, null);
+        Node node2 =new Node(args, null);
+        Node node3 =new Node(args, null);
+        Node node4 =new Node(args, null);
+        Node node5 =new Node(args, null);
+        node1.next = node2;
+        
+    }
+    
+}
